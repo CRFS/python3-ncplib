e144736a9538af1e214b7343ceab9c76a4d1287e
Dave Hall <dave@etianen.com>
2017-03-01 15:12:01 +0000

    Using no magic numbers

--- ncplib/packets.py
+++ ncplib/packets.py
@@ -13,13 +13,21 @@ FIELD_HEADER_STRUCT = Struct("<4s3s1sI")
 
 PARAM_HEADER_STRUCT = Struct("<4s3sB")
 
+PACKET_HEADER_SIZE = PACKET_HEADER_STRUCT.size
+
+FIELD_HEADER_SIZE = FIELD_HEADER_STRUCT.size
+
+PARAM_HEADER_SIZE = PARAM_HEADER_STRUCT.size
+
+PACKET_FOOTER_SIZE = 8
+
 
 # Packet encoding.
 
 def encode_packet(packet_type, packet_id, timestamp, info, fields):
     timestamp_unix, timestamp_nano = datetime_to_unix(timestamp)
     # Encode the header.
-    buf = bytearray(32)  # 32 is the size of the packet header.
+    buf = bytearray(PACKET_HEADER_SIZE)
     PACKET_HEADER_STRUCT.pack_into(
         buf, 0,
         b"\xdd\xcc\xbb\xaa",  # Hardcoded packet header.
@@ -30,7 +38,7 @@ def encode_packet(packet_type, packet_id, timestamp, info, fields):
         timestamp_unix, timestamp_nano,
         info,
     )
-    offset = 32  # 32 is the size of the packet header.
+    offset = PACKET_HEADER_SIZE
     # Write the packet fields.
     for field_name, field_id, params in fields:
         field_offset = offset
@@ -41,13 +49,13 @@ def encode_packet(packet_type, packet_id, timestamp, info, fields):
             b"\x00",  # Field type ID is ignored.
             field_id,
         ))
-        offset += 12  # 12 is the size of the field header.
+        offset += FIELD_HEADER_SIZE
         # Write the params.
         for param_name, param_value in params.items():
             # Encode the param value.
             param_type_id, param_encoded_value = encode_value(param_value)
             # Write the param header.
-            param_size = 8 + len(param_encoded_value)  # 8 is the size of the param header.
+            param_size = PARAM_HEADER_SIZE + len(param_encoded_value)
             param_padding_size = -param_size % 4
             buf.extend(PARAM_HEADER_STRUCT.pack(
                 param_name.encode("latin1"),
@@ -63,7 +71,7 @@ def encode_packet(packet_type, packet_id, timestamp, info, fields):
     # Encode the packet footer.
     buf.extend(b"\x00\x00\x00\x00\xaa\xbb\xcc\xdd")  # Hardcoded packet footer with no checksum.
     # Write the packet size.
-    buf[8:12] = ((offset + 8) // 4).to_bytes(4, "little")  # 8 is the size of the packet footer.
+    buf[8:12] = ((offset + PACKET_FOOTER_SIZE) // 4).to_bytes(4, "little")
     # All done!
     return buf
 
@@ -85,7 +93,7 @@ def decode_packet_cps(header_buf):
     if header != b"\xdd\xcc\xbb\xaa":  # pragma: no cover
         raise DecodeError("Invalid packet header {}".format(header))
     # Decode the rest of the body data.
-    size_remaining = size - 32  # 32 is the size of the packet header.
+    size_remaining = size - PACKET_HEADER_SIZE
 
     def decode_packet_body(buf):
         offset = 0
@@ -93,13 +101,13 @@ def decode_packet_cps(header_buf):
         if buf[-4:] != b"\xaa\xbb\xcc\xdd":  # pragma: no cover
             raise DecodeError("Invalid packet footer {}".format(buf[-4:]))
         # Decode fields.
-        field_limit = size_remaining - 8  # 8 is the size of the packet footer.
+        field_limit = size_remaining - PACKET_FOOTER_SIZE
         fields = []
         while offset < field_limit:
             # Decode field header.
             field_name, field_size, field_type_id, field_id = FIELD_HEADER_STRUCT.unpack_from(buf, offset)
             param_limit = offset + int.from_bytes(field_size, "little") * 4
-            offset += 12  # 12 is the size of the field header.
+            offset += FIELD_HEADER_SIZE
             # Decode params.
             params = {}
             while offset < param_limit:
@@ -112,7 +120,7 @@ def decode_packet_cps(header_buf):
                 param_name, param_size, param_type_id = PARAM_HEADER_STRUCT.unpack_from(buf, offset)
                 param_size = int.from_bytes(param_size, "little") * 4
                 # Decode the param value.
-                param_value_encoded = bytes(buf[offset+8:offset+param_size])  # 8 is the size of the param header.
+                param_value_encoded = bytes(buf[offset+PARAM_HEADER_SIZE:offset+param_size])
                 params[param_name.rstrip(b" \x00").decode("latin1")] = decode_value(param_type_id, param_value_encoded)
                 offset += param_size
                 # Check for param overflow.
@@ -138,5 +146,5 @@ def decode_packet_cps(header_buf):
 
 
 def decode_packet(buf):
-    body_size, decode_packet_body = decode_packet_cps(buf[:32])  # 32 is the size of the packet header.
-    return decode_packet_body(buf[32:])  # 32 is the size of the packet header.
+    body_size, decode_packet_body = decode_packet_cps(buf[:PACKET_HEADER_SIZE])
+    return decode_packet_body(buf[PACKET_HEADER_SIZE:])  # 32 is the size of the packet header.
