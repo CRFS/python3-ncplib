72831c037e17f8a620866454a0975df7d0783019
Dave Hall <dave@etianen.com>
2017-03-01 13:10:48 +0000

    Adding benchmark

---
+++ benchmarks/packets.py
@@ -0,0 +1,31 @@
+import array
+from datetime import datetime, timezone
+import timeit
+from ncplib.packets import encode_packet, decode_packet, PacketData, FieldData
+from ncplib import uint
+
+
+PACKET = PacketData("TEST", 1, datetime.now(tz=timezone.utc), b"INFO", [
+    FieldData("FIEL", 2, {
+        "INT": -99,
+        "UINT": uint(99),
+        "STR": "foo!",
+        "BYTE": b"bar!",
+        "ARR": array.array("i", [10]),
+    }),
+])
+
+
+def benchmark():
+    decoded = decode_packet(encode_packet(*PACKET))
+    assert decoded == PACKET, "{} != {}".format(decoded, PACKET)
+
+
+def main():
+    print("Starting packet encoding benchmark...")
+    result = min(timeit.repeat(benchmark, number=50000))
+    print("Result: {}".format(result))
+
+
+if __name__ == "__main__":
+    main()
