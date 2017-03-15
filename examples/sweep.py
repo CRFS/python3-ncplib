4477949133e251d185341ae64ec832452d75e690
Dave Hall <dave@etianen.com>
2017-03-15 11:15:07 +0000

    Added sweep example. Closes #13.

---
+++ examples/sweep.py
@@ -0,0 +1,31 @@
+"""
+NCP sweep data example.
+
+Connects to a node and requests a single sweep to be performed. Prints the result to stdout.
+"""
+import asyncio
+import ncplib
+
+
+# The node to connect to. Can be a DNS name or an IP address.
+NODE_HOST = "127.0.0.1"
+
+
+async def main():
+    """
+    The async main method.
+
+    Connects to a node and requests a single sweep to be performed. Prints the result to stdout.
+    """
+    # Connect to the node.
+    async with await ncplib.connect(NODE_HOST) as connection:
+        # Send a single DSPC command to the node.
+        response = connection.send("DSPC", "SWEP", FSTA=10000, FSTP=18000, INPT=1, BDEX=1, BINP=2)
+        # Wait for the node to reply.
+        field = await response.recv()
+        print(field)
+
+
+# Run the async main method if this file is run as a script.
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
